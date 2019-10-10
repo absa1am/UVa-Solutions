1dc0013fa22239130faa4a5f5dd985109b78a3d0
absa1am <salaam.mbstu@gmail.com>
2019-10-10 23:07:15 +0600

    1124 - Celebrity jeopardy

---
+++ Volume 011/UVa1124.cpp
@@ -0,0 +1,17 @@
+/*
+* Problem: 1124 - Celebrity jeopardy
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    string e;
+
+    while(getline(cin, e)) {
+        cout << e << "\n";
+    }
+    
+    return 0;
+}
