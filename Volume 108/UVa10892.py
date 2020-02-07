1d99b01e8c592f5b6f80f6850d3b085bb230c13e
Abdus Salam <salaam.mbstu@gmail.com>
2020-02-07 14:43:03 +0600

    10892 - LCM Cardinality

--- Volume 108/UVa10892.py
+++ Volume 108/UVa10892.py
@@ -9,21 +9,21 @@ def divisors(n):
 			if i != n // i:
 				d.append(n // i)
 		i = i + 1
-        return d
+	return d
 
 def gcd(a, b):
-        if b == 0:
+	if b == 0:
 		return a
 	else:
 		return gcd(b, a % b)
 
 n = int(input())
 while n != 0:
+	cnt = 1
 	x = divisors(n)
-	cnt = 0
 	for i in range(0, len(x)):
 		for j in range(i + 1, len(x)):
 			if (x[i] * x[j]) // gcd(x[i], x[j]) == n:
 				cnt = cnt + 1
-	print(n, cnt + 1)
+	print(n, cnt)
 	n = int(input())
