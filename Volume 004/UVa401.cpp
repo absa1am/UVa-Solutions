f9087258747c61aecadd7620cfcca98196834135
Abdus Salam <salaam.mbstu@gmail.com>
2020-02-21 16:26:30 +0600

    401 - Palindromes

--- Volume 004/UVa401.cpp
+++ Volume 004/UVa401.cpp
@@ -2,8 +2,33 @@
 * Problem: 401 - Palindromes
 */
 
-#include <bits/stdc++.h>
+#include <iostream>
+#include <map>
 using namespace std;
+
+map<char, char>m{
+                    {'A', 'A'},
+                    {'E', '3'},
+                    {'H', 'H'},
+                    {'I', 'I'},
+                    {'J', 'L'},
+                    {'L', 'J'},
+                    {'M', 'M'},
+                    {'O', 'O'},
+                    {'S', '2'},
+                    {'T', 'T'},
+                    {'U', 'U'},
+                    {'V', 'V'},
+                    {'W', 'W'},
+                    {'X', 'X'},
+                    {'Y', 'Y'},
+                    {'Z', '5'},
+                    {'1', '1'},
+                    {'2', 'S'},
+                    {'3', 'E'},
+                    {'5', 'Z'},
+                    {'8', '8'}
+                };
 
 bool isRegular(string s)
 {
@@ -12,22 +37,38 @@ bool isRegular(string s)
 
     while(i < j) {
         if(s[i] != s[j]) return false;
-        i++;
-        j--;
+        i++, j--;
     }
+
     return true;
 }
 
 bool isMirrored(string s)
 {
-    return false;
+    int i = 0;
+    int j = s.size() - 1;
+
+    while(i < j) {
+        if(s[i] != m[s[j]]) return false;
+        i++, j--;
+    }
+
+    if(s.size() % 2 && m[s[s.size() / 2]] != s[s.size() / 2]) return false;
+
+    return true;
 }
 
 int main()
 {
+    string s;
 
-    cout << isRegular("madbm") << "\n";
-    cout << isRegular("m") << "\n";
+    while(cin >> s) {
+        if(isRegular(s) && isMirrored(s)) cout << s << " -- is a mirrored palindrome.\n";
+        else if(isRegular(s)) cout << s << " -- is a regular palindrome.\n";
+        else if(isMirrored(s)) cout << s << " -- is a mirrored string.\n";
+        else cout << s << " -- is not a palindrome.\n";
+        cout << "\n";
+    }
 
     return 0;
 }
