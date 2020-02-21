b3e7b6fe211ca6e45290578e6ca8d11289ccd08a
Abdus Salam <salaam.mbstu@gmail.com>
2020-02-21 16:53:25 +0600

    11713 - Abstract Names

--- Volume 117/UVa11713.cpp
+++ Volume 117/UVa11713.cpp
@@ -5,8 +5,34 @@ Problem: 11713 - Abstract Names
 #include <bits/stdc++.h>
 using namespace std;
 
+bool isVowel(char c) {
+    return (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u');
+}
+
 int main()
 {
+    int t;
+
+    cin >> t;
+
+    while(t--) {
+        string a, b;
+
+        cin >> a >> b;
+
+        if(a.size() != b.size()) cout << "No\n";
+        else {
+            int i;
+
+            for(i = 0; i < a.size(); i++) {
+                if(!isVowel(a[i]) && a[i] != b[i]) break;
+                if(isVowel(a[i]) && !isVowel(b[i])) break;
+            }
+
+            if(i < a.size()) cout << "No\n";
+            else cout << "Yes\n";
+        }
+    }
 
     return 0;
 }
